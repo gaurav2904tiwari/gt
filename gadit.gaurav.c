aa61e99cad27e9d2f82464e9c71912678a68317c
gaurav2904tiwari <155514290+gaurav2904tiwari@users.noreply.github.com>
2024-01-03 11:51:29 +0530

    M S DHONI

---
+++ gadit.gaurav.c
@@ -0,0 +1,5 @@
+MAHANDER VSINGH DHONI is the only captain in the world who have won all 34 icc tournament .
+He has also recieved the rajiv gandhi khel ratna award.
+he has won 3 IPL tropies as captain of Chhenai super king.
+his nickname is Mahi.
+He is also called as Thala.
